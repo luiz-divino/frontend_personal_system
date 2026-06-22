8ce8d23cf789e96e859147b75765c20d05d28432
luiz-divino <nandodivinodev@gmail.com>
2026-06-22 13:34:46 -0300

    feat: create the structure for the registration page

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+export default function Register() {
+  return (
+    <div className="flex items-center justify-center min-h-screen px-4 py-8">
+      <div className="flex-1 bg-amber-400 flex justify-center w-full">
+        <h1>text container</h1>
+      </div>
+      <div className="hidden w-full mx-auto md:block flex-1 bg-amber-100 items-center justify-center ">
+        <h1>form container</h1>
+      </div>
+    </div>
+  );
+}
