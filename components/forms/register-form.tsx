55295397c587d99aeac852791ecee6f6594ce426
luiz-divino <nandodivinodev@gmail.com>
2026-06-22 18:21:07 -0300

    style: add color to text content

--- components/forms/register-form.tsx
+++ components/forms/register-form.tsx
@@ -13,25 +13,25 @@ export function RegisterForm() {
   return (
     <Card className="w-full max-w-md mx-auto bg-card-register">
       <CardHeader>
-        <CardTitle>Register</CardTitle>
-        <CardDescription>Enter your email below to create your account</CardDescription>
+        <CardTitle className="text-text-register">Personal Management</CardTitle>
+        <CardDescription className="text-amber-50">Enter your email below to create your account</CardDescription>
       </CardHeader>
       <CardContent>
         <form action="" className="space-y-4">
             <div className="space-y-2">
                 <Label htmlFor="name">Name</Label>
-                <Input id="name" type="text" placeholder="Digite seu nome..." minLength={3} required />
+                <Input className="bg-input-register placeholder:text-input-text-register" id="name" type="text" placeholder="Digite seu nome..." minLength={3} required />
             </div>
             <div className="space-y-2">
                     <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" placeholder="m@example.com" required />
+                    <Input className="bg-input-register placeholder:text-input-text-register" id="email" type="email" placeholder="m@example.com" required />
             </div>
             <div className="space-y-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="••••••••" minLength={6} required />
+                <Input className="bg-input-register placeholder:text-input-text-register" id="password" type="password" placeholder="••••••••" minLength={6} required />
             </div>
 
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full bg-text-register">
               Register
             </Button>
         </form>
