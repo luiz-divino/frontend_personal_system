bd48219c872df08870bc5bde52f2fa32c5c827a4
luiz-divino <nandodivinodev@gmail.com>
2026-06-30 20:24:02 -0300

    feat: create page of login structure
    - add style to structure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,15 @@
+import { LoginForm } from "@/components/forms/login-form";
+
 export default function Login() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Login Page</h1>
+        <div className="flex min-h-screen">
+            <div className="flex flex-1 items-center justify-center bg-card-register p-4">
+                <LoginForm />
+            </div>
+
+            <div className="hidden md:flex flex-1 items-center justify-center bg-amber-100 p-4">
+                <h1 className="text-2xl font-bold">Conteúdo de introdução</h1>
+            </div>
         </div>
     );
 }
