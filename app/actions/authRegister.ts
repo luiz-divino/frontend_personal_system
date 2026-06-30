4aec013b7f69570f6212852af176588caeac0274
luiz-divino <nandodivinodev@gmail.com>
2026-06-30 01:07:57 -0300

    feat: implement client API to receive data from the registration form
    - implement page redirect according to the received result
    - implement error and success handling

--- app/actions/authRegister.ts
+++ app/actions/authRegister.ts
@@ -1,18 +1,32 @@
 "use server";
+import { apiClient } from "@/lib/api";
 export async function registerFormAction(
-    prevState: {sucess: boolean, message: string} | null,
-    formData: FormData
-){
+    prevState: { sucess: boolean; message: string; redirectTo?: string } | null,
+    formData: FormData,
+) {
     console.log("registerFormAction", prevState, formData);
     const name = formData.get("name") as string;
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-    console.log("name", name);
-    console.log("email", email);
-    console.log("password", password);
 
-    return {
-        sucess: true,
-        message: "Cadastro realizado com sucesso!"
+    try {
+        const data = {
+            name: name,
+            email: email,
+            password: password,
+        };
+
+        await apiClient("/users", {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+
+        return { sucess: true, message: "User registered successfully!", redirectTo: "/login" };
+    } catch (error) {
+        if (error instanceof Error) {
+            return { sucess: false, message: error.message, redirectTo: "/register" };
+        }
+
+        return { sucess: false, message: "An unknown error occurred.", redirectTo: "/register" };
     }
 }
