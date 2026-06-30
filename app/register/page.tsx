565746f3575c84f80c64719b2f33c39af53191be
luiz-divino <nandodivinodev@gmail.com>
2026-06-30 20:35:48 -0300

    style: run editor config to formatter indentation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,15 +1,15 @@
 import { RegisterForm } from "@/components/forms/register-form";
 
 export default function Register() {
-  return (
-    <div className="flex min-h-screen">
-      <div className="flex flex-1 items-center justify-center bg-card-register p-4">
-        <RegisterForm />
-      </div>
+    return (
+        <div className="flex min-h-screen">
+            <div className="flex flex-1 items-center justify-center bg-card-register p-4">
+                <RegisterForm />
+            </div>
 
-      <div className="hidden md:flex flex-1 items-center justify-center bg-amber-100 p-4">
-        <h1 className="text-2xl font-bold">Conteúdo de introdução</h1>
-      </div>
-    </div>
-  );
+            <div className="hidden md:flex flex-1 items-center justify-center bg-amber-100 p-4">
+                <h1 className="text-2xl font-bold">Conteúdo de introdução</h1>
+            </div>
+        </div>
+    );
 }
