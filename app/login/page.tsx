54e3e4ef455a392b8615bfc2a2551cfa3d9df45f
luiz-divino <nandodivinodev@gmail.com>
2026-06-30 01:22:29 -0300

    chore: implement login page to test

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Login Page</h1>
+        </div>
+    );
+}
