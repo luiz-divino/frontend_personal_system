7abaa0efd4afdd43467f605de4a2ec71229c37ca
luiz-divino <nandodivinodev@gmail.com>
2026-06-30 23:12:52 -0300

    chore: create dashboard page to teste

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard(){
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-6xl">
+                AQUI É A PAGINA DE DASHBOARD
+            </h1>
+        </div>
+    )
+}
