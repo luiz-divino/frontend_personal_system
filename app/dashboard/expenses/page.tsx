95f193f71a4e11731d629c4adf083b6ec7afeeb4
luiz-divino <nandodivinodev@gmail.com>
2026-07-21 22:37:47 -0300

    chore: create page to test

---
+++ app/dashboard/expenses/page.tsx
@@ -0,0 +1,9 @@
+export default function Expenses(){
+    return (
+        <div>
+            <h1>
+                AQUI É A PAGINA DE DESPESAS
+            </h1>
+        </div>
+    )
+}
---
+++ app/dashboard/tasks/page.tsx
@@ -0,0 +1,9 @@
+export default function Tasks(){
+    return (
+        <div>
+            <h1>
+                AQUI É A PAGINA DE TASKS
+            </h1>
+        </div>
+    )
+}
