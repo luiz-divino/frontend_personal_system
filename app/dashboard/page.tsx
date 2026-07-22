547934985e4eb30e9722a42a9b911517146fbd57
luiz-divino <nandodivinodev@gmail.com>
2026-07-21 22:36:17 -0300

    chore: adjust dashboard page to test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function Dashboard(){
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <div className="flex min-h-dvh items-center justify-center">
             <h1 className="text-6xl">
                 AQUI É A PAGINA DE DASHBOARD
             </h1>
