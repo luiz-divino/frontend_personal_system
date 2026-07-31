dd9aaafec82e5b6be22cc5674fc87d459ebcc80a
luiz-divino <nandodivinodev@gmail.com>
2026-07-31 20:30:55 -0300

    feat: add responsive pie chart dashboard component
    - Create PieChartDashboard component using Recharts and Tailwind UI cards
    - Implement responsive container heights for mobile (h-64) and desktop (md:h-96)
    - Configure percentage-based inner and outer radii to improve layout and prevent label overlapping
    - Integrate formatCurrencyValue utility for external labels and internal tooltips
    - Define a 10-color palette array to dynamically style category slices

---
+++ components/(pages)/dashboard/pieChart.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { ExpenseCategorySummary } from "@/app/utils/amountByCategory";
+import { formatCurrencyValue } from "@/app/utils/formatCurrency";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+const COLORS = [
+  "#3b82f6",
+  "#10b981",
+  "#f59e0b",
+  "#ef4444",
+  "#8b5cf6",
+  "#5cf6e2",
+  "#173447",
+  "#e7f65c",
+  "#f65cf6",
+  "#158315",
+];
+
+export const PieChartDashboard = ({
+  amountByCategory,
+}: {
+  amountByCategory: ExpenseCategorySummary[];
+}) => {
+  return (
+    <Card className="w-full rounded-xl bg-app-card md:mx-auto p-4 shadow-sm select-none">
+      <CardHeader className="p-0">
+        <CardTitle className="text-gray-300 font-semibold">
+          Gastos por Categoria
+        </CardTitle>
+        <CardDescription>Visão detalhada</CardDescription>
+
+      </CardHeader>
+
+      <CardContent className="h-64 md:h-96 w-full p-0">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              data={amountByCategory}
+              dataKey="value"
+              nameKey="name"
+              innerRadius="40%"
+              outerRadius="65%"
+              paddingAngle={7}
+              label={({ value }) => `${formatCurrencyValue(value)}`}
+              labelLine={true}
+            >
+              {amountByCategory.map((entry, index) => (
+                <Cell
+                  key={`${entry.name}-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Tooltip
+              formatter={(value) => formatCurrencyValue(Number(value))}
+            />{" "}
+            <Legend verticalAlign="bottom" height={36} />
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+};
