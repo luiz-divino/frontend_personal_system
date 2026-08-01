6a8a8187234d33b8900df428a9f6ec90c34138ce
luiz-divino <nandodivinodev@gmail.com>
2026-08-01 18:24:59 -0300

    feat: conditionally render the TableTask when tasks exist or the NoData component with a prompt to add a new task when the list is empty  - Change text font size for total tasks

--- app/dashboard/tasks/page.tsx
+++ app/dashboard/tasks/page.tsx
@@ -1,13 +1,12 @@
 import { TaskForm } from "@/components/forms/task-form";
-import {
-  TasksFilter,
-} from "@/components/(pages)/tasks/TaskFilter";
+import { TasksFilter } from "@/components/(pages)/tasks/TaskFilter";
 import { getTasks, TaskStatus } from "@/lib/taskService";
 import { VALID_STATUSES } from "@/app/utils/task-mappings";
 import { getUser } from "@/lib/auth";
 import { HeaderCard } from "@/components/headerCard/headerCard";
 import { Welcome } from "@/components/welcome/welcomeUser";
 import { TableTask } from "@/components/(pages)/tasks/TableTask";
+import { NoData } from "@/components/noData";
 
 interface PageProps {
   searchParams: Promise<{ status?: string }>;
@@ -28,7 +27,7 @@ export default async function Tasks({ searchParams }: PageProps) {
       <HeaderCard>
         <Welcome name={user?.name} page={"Tarefas"} />
         <div className="hidden md:flex">
-        <TaskForm />
+          <TaskForm />
         </div>
       </HeaderCard>
       <section className="flex text-base md:text-xl items-center justify-between text-white">
@@ -42,7 +41,7 @@ export default async function Tasks({ searchParams }: PageProps) {
       <section className="flex items-center justify-center">
         <TasksFilter />
       </section>
-      <TableTask data={tasks} />
+      {tasks.length !== 0 ? <TableTask data={tasks} /> : <NoData page={"Tarefas"} message={"Clique em adicionar nova Tarefa"}/>}
     </main>
   );
 }
